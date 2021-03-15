cfd6209d445bd829b220f60cacf1713eefadaab9
SiroBg <borisusik@yandex.ru>
2021-03-16 01:03:47 +0300

    feat: добавить бургер меню

--- app/js/index.js
+++ app/js/index.js
@@ -3,3 +3,18 @@ $(document).on('click', 'a[href^="#"]', function (event) {
       scrollTop: $($.attr(this, 'href')).offset().top
   }, 500);
 });
+
+const hamburgerButton = document.querySelector('.hamburger');
+const navMenu = document.querySelector('.menu');
+
+hamburgerButton.addEventListener('click', () => {
+  hamburgerButton.classList.toggle('active');
+  navMenu.classList.toggle('menu_active')
+})
+
+navMenu.addEventListener('click', evt => {
+  if(evt.target.classList.contains('menu__link') && evt.currentTarget.classList.contains('menu_active')) {
+    hamburgerButton.classList.remove('active');
+    navMenu.classList.remove('menu_active')
+  }
+})
